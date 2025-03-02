71c6e2f295ed1b2911028e99bd1d4921aeec2666
rosa ovalle <you@example.com>
2025-03-02 13:50:18 -0800

    added task utility and menu for program

--- final/Program.cs
+++ final/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          List<Task> tasks = new List<Task>();
+            while (true)
+            {
+                Console.WriteLine("Task Managent Sytem");
+                int menu = 0;
+                Console.WriteLine("Please choose an option");
+                Console.WriteLine("1 Add a Work Task: ");
+                Console.WriteLine("2 Add a School Task: ");
+                Console.WriteLine("3 Display all Task : ");
+                 Console.WriteLine("4 Exit");
+                Console.Write("Your choice: ");
+            }
         }
     }
 }
