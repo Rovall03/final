5f6fd4a9b0993077d7b474a2f63eb4511d2fc4ff
rosa ovalle <you@example.com>
2025-03-01 19:36:23 -0800

    Add project files.

---
+++ final/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ final/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final
+{
+ public abstract class Task
+    {
+       public string TaskName { get; set; }
+        public string TaskDue { get; set; }
+        public string TaskType { get; set; }
+
+        public Task() { }
+
+
+    }
+}
