c11d2cac55b75ac562398cc59d8ef4d3e30728e5
rosa ovalle <you@example.com>
2025-03-02 14:14:24 -0800

    added 4 remove item

--- final/Program.cs
+++ final/Program.cs
@@ -13,8 +13,26 @@
                 Console.WriteLine("1 Add a Work Task: ");
                 Console.WriteLine("2 Add a School Task: ");
                 Console.WriteLine("3 Display all Task : ");
-                 Console.WriteLine("4 Exit");
+                Console.WriteLine("4 Romove Task from List ");
+                 Console.WriteLine("5 Exit");
                 Console.Write("Your choice: ");
+
+                try
+                { menu = int.Parse(Console.ReadLine());
+                }
+                catch 
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    continue;
+                }
+
+                if (menu ==1)
+                {
+                    WorkTask workTask = new WorkTask();
+                    Console.Write("Work Task Name: ");
+                    workTask.TaskName = Console.ReadLine();
+                                        
+                }
             }
         }
     }
